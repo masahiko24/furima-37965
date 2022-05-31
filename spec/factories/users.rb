d18b384da8e8c7f41fa575c11e7986036f273dc0
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-05-31 14:47:43 +0900

    テストコード作成の準備完了

---
+++ spec/factories/users.rb
@@ -0,0 +1,20 @@
+
+
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name            {Faker::Name.first_name}
+    last_name             {Faker::Name.last_name}
+    first_name_reading    {'ハジメ'}
+    last_name_reading     {'タナカ'}
+    birthday              {Faker::Date.between(from: '2014-09-23', to: '2014-09-25') }
+  end
+end
+
+
+
+
+ 
