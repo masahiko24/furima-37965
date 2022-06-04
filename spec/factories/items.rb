747b5d3c157cc340598f4e5cfdf1d8ac0b95b582
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-06-04 12:11:20 +0900

    テストコード準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
+    image                     {'IMG-4352.jpg'}
+    brandname                 {'ut'}
+    description               {'nice'}
+    details_category          {'メンズ'}
+    details_condition         {'新品'}
+    delivery_charge           {'着払い'}
+    delivery_area             {'福岡県'}
+    delivery_number_of_date   {'1〜2日で発送'}
+    price                     {'100'}
+    price_is_invalid          {}
     
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+before do
+  @item = FactoryBot.build(:item)
+end
+
+  describe '商品登録' do
+
+    context '商品登録が上手く行く時' do
+      it "image,brandname,description,details_category,details_condition,delivery_charge,delivery_area,delivery_number_of_date,price,price_is_invalidが存在すれば登録できる" do
+        expect(item).to be_valid
+      end
+  
+  it '商品画像を1枚つけることが必須であること。'
+  @item.image = ''
+  @item.valid?
+  binding.pry
+
+  it '商品名が必須であること。'
+  @item.brandname = ''
+  @item.valid?
+
+  it '商品の説明が必須であること。'
+  @item.description = ''
+  @item.valid?
+
+  it 'カテゴリーの情報が必須であること。'
+  @item.details_category = ''
+  @item.valid?
+
+  it '商品の状態の情報が必須であること。'
+  @item.details_condition  = ''
+  @item.valid?
+
+  it '配送料の負担の情報が必須であること。'
+  @item.elivery_charge = ''
+  @item.valid?
+
+  it '発送元の地域の情報が必須であること。'
+  @item.delivery_area = ''
+  @item.valid?
+
+  it '発送までの日数の情報が必須であること。'
+  @item.delivery_number_of_date = ''
+  @item.valid?
+
+  it '価格の情報が必須であること。'
+  @item.price= ''
+  @item.valid?
+
+  it '価格は、¥300~¥9,999,999の間のみ保存可能であること。'
+  @item.price_is_invalid = ''
+  @item.valid?
+
+  it '価格は半角数値のみ保存可能であること。'
+  @item.price = ''
+  @item.valid?
+
 end
