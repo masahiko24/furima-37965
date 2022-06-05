32a588f5ce4481ad272f262580b7b821944b6b5d
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-06-05 14:14:09 +0900

    商品出品機能：修正版２

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,11 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :brandname, presence: true
   validates :description, presence: true
-  validates :details_category_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :details_condition_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :delivery_charge_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :delivery_area_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :delivery_number_of_date_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :details_category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :details_condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_number_of_date_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
   validates :price, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
