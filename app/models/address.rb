c6d162c59c81c855eb6df8139e5aee144884522a
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-06-10 19:44:08 +0900

    Formオブジェクト作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,11 +2,4 @@ class Address < ApplicationRecord
 
   belongs_to :order
 
-  validates :postal_code, presence: true
-  validates :delivery_area_id, presence: true
-  validates :city, presence: true
-  validates :house_number, presence: true
-  validates :bilding_name, presence: true
-  validates :phone_number, presence: true
-
 end
