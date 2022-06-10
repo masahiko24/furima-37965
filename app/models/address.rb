5c1099dfecad686ef5a5186976623bf6a07fd0ad
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-06-10 16:50:34 +0900

    アソシエーションのbuyをorderに変更

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,12 @@
 class Address < ApplicationRecord
 
-  belongs_to :buy
+  belongs_to :order
 
   validates :postal_code, presence: true
   validates :delivery_area_id, presence: true
   validates :city, presence: true
   validates :house_number, presence: true
-  validates :bilding_name, presence: true, :allow_nil
+  validates :bilding_name, presence: true
   validates :phone_number, presence: true
 
 end
