0c1c10481c69c38aebb01508f9db66f1c4e69acc
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-06-11 17:20:57 +0900

    tokenのバリデーション実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,23 +1,18 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :delivery_area_id, :city, :house_number, :bilding_name, :phone_number, :order_id, :user_id, :item_id
+  attr_accessor :postal_code, :delivery_area_id, :city, :house_number, :bilding_name, :phone_number, :order_id, :user_id, :item_id, :token
 
   with_options presence: true do
   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-
   validates :delivery_area_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :city
   validates :house_number
-  validates :bilding_name
   validates :phone_number
-  validates :order_id
-  validates :user_id
-  validates :item_id
-
+  validates :token
   end
 
   def save 
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, delivery_area_id: delivery_area_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, delivery_area_id: delivery_area_id, city: city, house_number: house_number, bilding_name: bilding_name, phone_number: phone_number, order_id: order.id)
   end
 end
