8873fa1a94947c68f8dedd81f10530a98ff933f9
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-06-11 13:16:38 +0900

    決済機能のクライアントサイド実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,29 +1,32 @@
-// const pay = () => {
-//   Payjp.setPublicKey(
-//   "pk_test_968ae258a720fca198e796e7")
-//   const submit = document.getElementById("button");
-//   submit.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     const formResult = document.getElementById("charge-form");
-//     const formData = new FormData(formResult);
+const pay = () => {
+  
+  Payjp.setPublicKey(
+  "pk_test_968ae258a720fca198e796e7")
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
 
-//     const card = {
-//       number: formData.get("order[number]"),
-//       cvc: formData.get("order[cvc]"),
-//       exp_month: formData.get("order[exp_month]"),
-//       exp_year: `20${formData.get("order[exp_year]")}`,
-//     };
-//     Payjp.createToken(card, (status, response) => {
-//       if (status == 200) {
-//         const token = response.id;
-//         const renderDom = document.getElementById("charge-form");
-//         const tokenObj = `<input value=${token} name='token'>`;
-//         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-//         debugger;
-//         console.log(token)
-//       }
-//     });
-//   });
-// };
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
 
-// window.addEventListener("load", pay);
+    const card = {
+      number: formData.get("order_address[card_number]"),
+      cvc: formData.get("order_address[card_security_code]"),
+      exp_month: formData.get("order_address[card_month]"),
+      exp_year: `20${formData.get("order_address[card_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        debugger;
+        console.log(token)
+      }
+});
+});
+    };
+
+window.addEventListener("load", pay);
