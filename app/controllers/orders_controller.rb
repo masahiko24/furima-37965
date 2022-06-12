3737897f760d303e5e3d356c81fe9088d4108b85
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-06-12 20:34:14 +0900

    インデント修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,21 +3,19 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   before_action :move_to_index, only: [:index]
 
-  def index 
+  def index
     @order_address = OrderAddress.new
-
   end
 
   def new
   end
-
 
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
       @order_address.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render 'index'
     end
@@ -26,25 +24,25 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :delivery_area_id, :city, :house_number, :bilding_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :delivery_area_id, :city, :house_number, :bilding_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
-   def set_item
-    @item = Item.find(params[:item_id])   
+  def set_item
+    @item = Item.find(params[:item_id])
   end
-  
+
   def move_to_index
-   if current_user.id == @item.user_id || @item.order.present?
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-        amount: @item.price,  # 商品の値段
-        card: order_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
-    end
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,20 +1,22 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :delivery_area_id, :city, :house_number, :bilding_name, :phone_number, :order_id, :user_id, :item_id, :token
+  attr_accessor :postal_code, :delivery_area_id, :city, :house_number, :bilding_name, :phone_number, :order_id, :user_id,
+                :item_id, :token
 
   with_options presence: true do
-  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :city
-  validates :house_number
-  validates :phone_number, format: { with: /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/ }
-  validates :token
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :city
+    validates :house_number
+    validates :phone_number,
+              format: { with: /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0-?\d{4}-?\d{4}\z/ }
+    validates :token
   end
 
   validates :delivery_area_id, numericality: { other_than: 0, message: "can't be blank" }
 
-
-  def save 
+  def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, delivery_area_id: delivery_area_id, city: city, house_number: house_number, bilding_name: bilding_name, phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, delivery_area_id: delivery_area_id, city: city, house_number: house_number,
+                   bilding_name: bilding_name, phone_number: phone_number, order_id: order.id)
   end
 end
